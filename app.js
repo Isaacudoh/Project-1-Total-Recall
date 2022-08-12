25df911b5a82f36936c37b96dbf3024fe5f10c4f
nsikakudoh <nsikak@sparknerds.io>
2022-08-12 04:33:21 -0600

    Comple Readme file

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function checkForMatch(){
             firstSelection.classList.remove("flip");
             secondSelection.classList.remove("flip");
             resetBoard()
-        }, 1000);
+        }, 8000);
     }
 }
 
